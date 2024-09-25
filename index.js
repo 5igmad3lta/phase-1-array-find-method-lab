1147a58c4df09464ecfcc337c964c25c883bc2f4
Stavros Droulias <stavros.d27@gmail.com>
2024-09-25 23:48:34 +1000

    complete lab

--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 
-function superbowlWin(record) {
-       const win = record.find(game => game.result === 'W');
-       return win ? win.year : undefined;
+function superbowlWin(records) {
+    const winningGame = records.find(gameRecord => gameRecord.result === 'W')
+    return winningGame && winningGame.year
 }
-  
